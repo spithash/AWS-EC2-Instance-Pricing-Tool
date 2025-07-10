23c92edde0e1166da2e8af70a9aeaa9058fd813f
Stathis Xantinidis <spithash@tuta.io>
2025-07-10 18:55:56 +0000

    version 0.1.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aws-ec2-pricing-tool",
-    version="0.1.1",
+    version="0.1.2",
     author="Stathis Xantinidis",
     author_email="spithash@tuta.io",
     description="CLI tool to compare AWS EC2 on-demand pricing and AMIs across regions",
